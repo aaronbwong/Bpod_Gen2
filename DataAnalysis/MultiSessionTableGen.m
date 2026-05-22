7ba34c5673f38b67ef73c12da4a11cd3efe6c515
Aaron Benson Wong <111995444+aaronbwong@users.noreply.github.com>
2026-05-22 10:16:11 +0200

    added default folder path for Multisession workflow

--- DataAnalysis/MultiSessionTableGen.m
+++ DataAnalysis/MultiSessionTableGen.m
@@ -1,8 +1,7 @@
 %% Multi-session table generation
 % Select session file paths, then run analysis on the selected files.
-clearvars
-
-selectedFiles = multi_session.selectFiles();
+load('DEFAULT_FILE_PATHS.mat','DEFAULT_DATA_FOLDER')
+selectedFiles = multi_session.selectSubjectFolders(DEFAULT_DATA_FOLDER);
 if isempty(selectedFiles)
     disp('No files selected. Exiting.');
     return;
